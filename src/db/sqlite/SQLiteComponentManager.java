ca9eb1c7d03aea848be36965f23924777e5b5e49
drijc <drijc@LAPTOP-G4ANKLAT>
2020-03-24 00:01:28 +0100

    actualizacion

---
+++ src/db/sqlite/SQLiteComponentManager.java
@@ -0,0 +1,28 @@
+package db.sqlite;
+
+import java.util.List;
+
+import db.interfaces.ComponentManager;
+import db.pojos.Component;
+
+public class SQLiteComponentManager implements ComponentManager {
+
+	@Override
+	public void add(Component component) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Component> searchByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Component> searchBySupplier(String supplier) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
