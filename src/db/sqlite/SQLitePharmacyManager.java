c6dec51e04aef34c1fe8e1ee3b8ed8f27626a932
RAQUEL <RAQUEL@LAPTOP-BEI1SPFG.Home>
2020-03-23 20:26:30 +0100

    changes

--- src/db/sqlite/SQLitePharmacyManager.java
+++ src/db/sqlite/SQLitePharmacyManager.java
@@ -1,17 +1,43 @@
 package db.sqlite;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
-import db.pojos.*;
+
 
 import db.interfaces.PharmacyManager;
-import db.pojos.Pharmacy;
 
+import db.pojos.Pharmacy;
 public class SQLitePharmacyManager implements PharmacyManager {
+	private Connection c;
+	public SQLitePharmacyManager(Connection c) {
+		this.c=c;
+	}
+
 
 	@Override
 	public List<Pharmacy> searchByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		List <Pharmacy> pharmacyList = new ArrayList <Pharmacy> ();
+		try {
+			String sql = "SELECT * FROM pharmacy WHERE name LIKE ?";
+			PreparedStatement prep = c.prepareStatement(sql);
+			prep.setString(1, "%"+name+"%");
+			ResultSet rs = prep.executeQuery();
+			while (rs.next()) {
+			int id = rs.getInt("id");
+			String pharmacyName = rs.getString("name");
+			int pharmacyContract_pid = rs.getInt("contrac_pid");
+			String pharmacyLocation = rs.getString("location");
+			Pharmacy newpharmacy = new Pharmacy(pharmacyName,pharmacyContract_pid,pharmacyLocation);
+		   
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return pharmacyList;
+	}
 	}
 
-}
+
