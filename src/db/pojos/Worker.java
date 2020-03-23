ddb2b57b102a5691a48efe64fadf3d8622017c51
pablo <pablo@DESKTOP-UOFBLG6>
2020-03-23 17:31:26 +0100

    Worker pojo finished, hope it's right

--- src/db/pojos/Worker.java
+++ src/db/pojos/Worker.java
@@ -1,5 +1,116 @@
 package db.pojos;
 
-public class Worker {
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class Worker implements Serializable {
+
+	private static final long serialVersionUID = 8022340278172396644L;
+	
+	private Integer id;
+	private String name;
+	private String position;
+	private Date start_date;
+	private String nationality;
+	private Integer contract_id;
+	
+	public Worker() {
+		super();
+	}
+	
+	public Worker (String name, String position, Date start_date, String nationality, Integer contract_id) {
+		super();
+		this.name = name;
+		this.position = position;
+		this.start_date = start_date;
+		this.nationality = nationality;
+		this.contract_id = contract_id;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Worker [id=" + id + ", name=" + name + ", position=" + position + ", start_date=" + start_date
+				+ ", nationality=" + nationality + ", contract_id=" + contract_id + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPosition() {
+		return position;
+	}
+	
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	public Date getStart_date() {
+		return start_date;
+	}
+	
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	
+	public String getNationality() {
+		return nationality;
+	}
+	
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	
+	public Integer getContract_id() {
+		return contract_id;
+	}
+	
+	public void setContract_id(Integer contract_id) {
+		this.contract_id = contract_id;
+	}
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
 }
