92e2c2553aadb0340cb9aadfd4995e99a2d0cf8b
RAQUEL <RAQUEL@LAPTOP-BEI1SPFG.Home>
2020-03-23 17:39:20 +0100

    changes

--- src/db/interfaces/ProductManager.java
+++ src/db/interfaces/ProductManager.java
@@ -1,5 +1,17 @@
 package db.interfaces;
 
-public class ProductManager {
+import db.pojos.Products;
 
+import java.util.List;
+
+public interface ProductManager {
+
+
+	 public List<Products> searchByName(String name);
+	 public List<Products> searchByType(String type);
+	 public List<Products> searchByPrice(Float price);
+	 public void add(Products product);
+	 public void buy(Products product);
+	
+	 
 }
