eace2b817ea93a936015fd704e6f2fd511826842
pablo <pablo@DESKTOP-UOFBLG6>
2020-03-27 20:02:34 +0100

    Ahora sí

---
+++ src/db/sqlite/SQLiteWorkerManager2.java
@@ -0,0 +1,105 @@
+package db.sqlite;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.interfaces.WorkerManager;
+import db.pojos.Worker;
+
+public class SQLiteWorkerManager2 implements WorkerManager {
+
+	private Connection c;
+
+	public SQLiteWorkerManager2(Connection c) {
+		this.c = c;
+	}
+
+	@Override
+	public List<Worker> searchByName(String name) {
+		// TODO Auto-generated method stub
+		// return null;
+		List<Worker> workersList = new ArrayList<Worker>();
+		try {
+			String sql = "SELECT * from workers WHERE name LIKE ?";
+			PreparedStatement prep = c.prepareStatement(sql);
+			prep.setString(1, "%" + name + "%");
+			ResultSet rs = prep.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String workerName = rs.getString("name");
+				String workerPosition = rs.getString("position");
+				Date workerStartDate = rs.getDate("start_date");
+				String workerNationality = rs.getString("nationality");
+				Integer workerContract_id = rs.getInt("contract_id");
+				Worker newworker = new Worker(workerName, workerPosition, workerStartDate, workerNationality,
+						workerContract_id);
+				workersList.add(newworker);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return workersList;
+	}
+
+	@Override
+	public List<Worker> searchByPosition(String position) {
+		// TODO Auto-generated method stub
+		// return null;
+		List<Worker> workersList = new ArrayList<Worker>();
+		try {
+			String sql = "SELECT * from workers WHERE position LIKE ?";
+			PreparedStatement prep = c.prepareStatement(sql);
+			prep.setString(1, "%" + position + "%");
+			ResultSet rs = prep.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String workerName = rs.getString("name");
+				String workerPosition = rs.getString("position");
+				Date workerStartDate = rs.getDate("start_date");
+				String workerNationality = rs.getString("nationality");
+				Integer workerContract_id = rs.getInt("contract_id");
+				Worker newworker = new Worker(workerName, workerPosition, workerStartDate, workerNationality,
+						workerContract_id);
+				workersList.add(newworker);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return workersList;
+	}
+
+	@Override
+	public void add(Worker worker) {
+		// TODO Auto-generated method stub
+		try {
+			String sql = "INSERT INTO workers (name, position, start_date, nationality, contract_id) "
+					+ "Values (?,?,?,?,?);";
+			PreparedStatement prep = c.prepareStatement(sql);
+			prep.setString(2, worker.getName());
+			prep.setString(3, worker.getPosition());
+			prep.setDate(4, worker.getStart_date());
+			prep.setString(5, worker.getNationality());
+			prep.setInt(6, worker.getContract_id());
+			prep.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	@Override
+	public void fire(Worker worker) {
+		// TODO Auto-generated method stub
+		try {
+			String sql = "DELETE FROM workers WHERE name LIKE ?";
+			PreparedStatement prep = c.prepareStatement(sql);
+			prep.setString(1, "%" + worker + "%");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -1,11 +1,26 @@
 package ui;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Menu {
 	private static BufferedReader reader;
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Welcome");
+		System.out.println("Please type your kind of user:");
+		System.out.println("1. Boss");
+		System.out.println("2. Worker");
+		System.out.println("3. Pharmacy");
+		int choice = Integer.parseInt(reader.readLine());
+		switch(choice) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		}
 	}
 
 }
